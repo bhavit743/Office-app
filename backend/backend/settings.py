bf0f61d708fcf8dd53e9523bd647d5c4891a6288
Bhavit Gupta <bhavitgupta@Bhavits-MacBook-Air.local>
2025-10-19 23:52:01 +0530

    back cors fix5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -143,7 +143,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "bhavitgupta743@gmail.com"
 EMAIL_HOST_PASSWORD = "bxkwqsqtvgcgjpwu"  # use App Password, not raw Gmail password
+
+EMAIL_USE_SSL = True
+
