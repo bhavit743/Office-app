7326b33611162f1fb4e2ded6ff71155d22517246
Bhavit Gupta <bhavitgupta@Bhavits-MacBook-Air.local>
2025-10-22 02:20:12 +0530

    storage fix 6

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,6 +18,7 @@ import google.oauth2.service_account
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+GS_DEFAULT_ACL = 'publicRead'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
